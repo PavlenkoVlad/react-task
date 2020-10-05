fd4a7ba921ed4030bbdd1592865abbd35cce97a2
a5-ios <alexk@a5.ua>
2020-10-06 01:32:01 +0300

    react-task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import ResultComponent from "./components/ResultComponent";
+import KeyboardComponent from "./components/KeyboardComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      result: ""
+    }
+  }
+
+  calculate = () => {
+    try {
+      this.setState({
+        result: (eval(this.state.result) || "0" ) + ""
+      });
+    } catch(e) {
+      this.setState({
+        result: "Error"
+      })
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  };
+
+  save = () => {
+    const element = document.createElement("a");
+    const file = new Blob([this.state.result], {type: 'text/plain'});
+    element.href = URL.createObjectURL(file);
+    element.download = "result.txt";
+    document.body.appendChild(element);
+    element.click();
+  };
+
+  onClick = button => {
+    if (button === "=") {
+      this.calculate();
+    } else if (button === "C") {
+      this.reset();
+    } else if (button === "CE") {
+      this.backspace();
+    } else if (button === "Save to file") {
+      this.save();
+    } else {
+      this.setState({
+        result: this.state.result + button
+      });
+    }
+  };
+
+
+  render() {
+    return(
+        <div>
+          <div className="calculator">
+            <ResultComponent  result={this.state.result}/>
+            <KeyboardComponent onClick={this.onClick}/>
+          </div>
+        </div>
+    );
+  }
+
+
+
 }
 
 export default App;
---
+++ src/components/KeyboardComponent.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+
+class KeyboardComponent extends Component {
+
+    render() {
+        return(
+            <div className="keyboard">
+                <button className="option-button" name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
+                <button className="option-button" name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
+                <button className="option-button" name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
+                <button className="option-button" name="C" onClick={e => this.props.onClick(e.target.name)}>C</button><br/>
+
+                <button className="digit-button" name="7" onClick={e => this.props.onClick(e.target.name)}>7</button>
+                <button className="digit-button" name="8" onClick={e => this.props.onClick(e.target.name)}>8</button>
+                <button className="digit-button" name="9" onClick={e => this.props.onClick(e.target.name)}>9</button>
+                <button className="operation-button" name="*" onClick={e => this.props.onClick(e.target.name)}>*</button><br/>
+
+                <button className="digit-button" name="4" onClick={e => this.props.onClick(e.target.name)}>4</button>
+                <button className="digit-button" name="5" onClick={e => this.props.onClick(e.target.name)}>5</button>
+                <button className="digit-button" name="6" onClick={e => this.props.onClick(e.target.name)}>6</button>
+                <button className="operation-button" name="-" onClick={e => this.props.onClick(e.target.name)}>-</button><br/>
+
+                <button className="digit-button" name="1" onClick={e => this.props.onClick(e.target.name)}>1</button>
+                <button className="digit-button" name="2" onClick={e => this.props.onClick(e.target.name)}>2</button>
+                <button className="digit-button" name="3" onClick={e => this.props.onClick(e.target.name)}>3</button>
+                <button className="operation-button" name="+" onClick={e => this.props.onClick(e.target.name)}>+</button><br/>
+
+                <button className="digit-button" name="." onClick={e => this.props.onClick(e.target.name)}>.</button>
+                <button className="digit-button" name="0" onClick={e => this.props.onClick(e.target.name)}>0</button>
+                <button className="digit-button" name="=" onClick={e => this.props.onClick(e.target.name)}>=</button>
+                <button className="operation-button" name="/" onClick={e => this.props.onClick(e.target.name)}>÷</button><br/>
+
+                <button className="save-button" name="Save to file" onClick={e => this.props.onClick(e.target.name)}>Save to file</button><br/>
+            </div>
+        );
+    }
+}
+
+export default KeyboardComponent;
